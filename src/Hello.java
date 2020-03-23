dc0fa1bc6f084579807ed7b7cbec8a19569d5b46
Duanhuiwen7 <fanghai.dhw@raycloud.com>
2020-03-23 16:28:32 +0800

    add dateformat

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,41 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws ParseException {
+        for(int i =0;i<40;i++){
+            System.out.println("CREATE TABLE `erp_lock_"+i+"` ( `key` varchar(128) NOT NULL, `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `total` int(8) DEFAULT '1', PRIMARY KEY (`key`) ) ENGINE = InnoDB CHARSET = utf8;");
+        }
+
+
+//        System.out.println("Hello World");
+//
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//
+//
+//        System.out.println(minTimeFilter("2000-1-1 00:00:00",dateFormat));
+//        System.out.println(minTimeFilter("2001-1-1 00:00:00",dateFormat));
+//        System.out.println(minTimeFilter("1999-1-1 00:00:00",dateFormat));
     }
+
+
+
+    public  static Date minTimeFilter(String payDate, SimpleDateFormat dateFormat) throws ParseException {
+
+        Calendar minDate = Calendar.getInstance();
+        minDate.setTime(dateFormat.parse("2000-1-1 00:00:00"));
+
+        Calendar payTime = Calendar.getInstance();
+        payTime.setTime(dateFormat.parse(payDate));
+
+        payTime = payTime.before(minDate) ? minDate : payTime;
+
+
+        return payTime.getTime();
+
+    }
+
+
 }
